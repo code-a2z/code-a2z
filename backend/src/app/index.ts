98205362ef5b913d70e65740a304248fdf765df0
Avdhesh-Varshney <avdhesh7v@gmail.com>
2026-02-28 10:24:11 +0530

    Add cookie-parser and cors middleware to Express app; update package.json and package-lock.json with new dependencies

--- backend/src/app/index.ts
+++ backend/src/app/index.ts
@@ -1,7 +1,21 @@
 import express from 'express';
 import type { Request, Response } from 'express';
+import apiRouter from './api/index.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  })
+);
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
@@ -9,5 +23,7 @@ app.get('/', (req: Request, res: Response) => {
     status: 'ok',
   });
 });
+
+app.use('', apiRouter);
 
 export default app;
